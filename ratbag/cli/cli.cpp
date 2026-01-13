ec7004d42e05c7bb84e6c112f82b9a8df7fe9a1d
Iliya Guterman <amfernusus@gmail.com>
2026-01-13 09:52:14 +0200

    wip - compiles cli

--- ratbag/cli/cli.cpp
+++ ratbag/cli/cli.cpp
@@ -3,13 +3,20 @@
 #include "ratbag/lib/libratbag.hpp"
 
 using ratbag::lib::Device;
+using ratbag::lib::hidapi::HIDDeviceInfo;
 
 int main() {
-  // 1. List all HID devices
+  auto deviceInfos = HIDDeviceInfo::enumerate_hid_devices();
+  for (auto &info : deviceInfos) {
+    auto [vid, pid] = info.device_id();
+    std::cout << "vid = " << vid << "pid = " << pid;
+  }
+
+  // std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 
   // 2. Open Device with corresponding driver to the HID based on HID
   // description and Path
-  Device<Driver>::Open();
+  // Device<Driver>::Open();
 
   std::cout << "Hello, Modern C++ World!" << std::endl;
 
