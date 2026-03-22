04efd38687b2b8a20024d574518da84c822e23d3
ilya guterman <amfernusus@gmail.com>
2026-03-22 23:19:35 +0900

    wip

--- ratbag/lib/device.hpp
+++ ratbag/lib/device.hpp
@@ -7,14 +7,34 @@
 namespace ratbag {
 namespace lib {
 
+class Device;
+
+using DeviceName = std::string_view;
+using DeviceList = std::vector<Device>;
+
 class Device {
 public:
-  static Device Open();
+  
+  DeviceName device_name();
 
-  void Read();
+  static DeviceList list_devices() {
+    // TODO: arene_base type list to iterate over types,
+    //       type list is not part of C++ standart, maybe use tuple 
+    for ()
+    // TODO: for each driver, call the static probe function to check for availale devices
+    //       each driver can support multiple device models
+    
+  }
 
-  void Write();
+private:
+  Driver _driver;
 
+  // void Read();
+
+  // void Write();
+
+  // TOOD: device has profiles -> profile has buttons, led, sensor
+  //       we will start with LEDs
   std::vector<Profile> GetProfiles();
 };
 } // namespace lib
